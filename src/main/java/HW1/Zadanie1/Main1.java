eade45975f2b5e3ffbaac6b7f36d7b2bf4d4e52f
oleg <Se2uks1n@gmail.com>
2023-04-11 22:35:57 +0300

    Commit add

---
+++ src/main/java/HW1/Zadanie1/Main1.java
@@ -0,0 +1,11 @@
+package HW1.Zadanie1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        printEvenAndOdd(nums);
+    }
+
+    private static void printEvenAndOdd(int[] nums) {
+    }
+}
